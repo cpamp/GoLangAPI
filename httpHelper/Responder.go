4c6c0eeeae74c0ada991ad187ade3ea5f1a75778
Pamperin, Clint A <c.pamperin21@gmail.com>
2017-04-01 23:41:27 -0500

    add: more status codes

--- httpHelper/Responder.go
+++ httpHelper/Responder.go
@@ -85,9 +85,74 @@ func (r Responder) getResponseText(resp interface{}) string {
 	return result
 }
 
+func (r Responder) write(a interface{}, code int) {
+	r.HTTPWriter.WriteHeader(code)
+	r.HTTPWriter.Write([]byte(r.getResponseText(a)))
+}
+
 // Ok Return ok
 func (r Responder) Ok(a interface{}) {
-	r.HTTPWriter.Write([]byte(r.getResponseText(a)))
+	r.write(a, http.StatusOK)
+}
+
+// Created created
+func (r Responder) Created(a interface{}) {
+	r.write(a, http.StatusCreated)
+}
+
+// Accepted accepted
+func (r Responder) Accepted(a interface{}) {
+	r.write(a, http.StatusAccepted)
+}
+
+// NonAuthoritativeInformation non authoritative information
+func (r Responder) NonAuthoritativeInformation(a interface{}) {
+	r.write(a, http.StatusNonAuthoritativeInfo)
+}
+
+// NoContent no content
+func (r Responder) NoContent(a interface{}) {
+	r.write(a, http.StatusNoContent)
+}
+
+// PartialContent partial content
+func (r Responder) PartialContent(a interface{}) {
+	r.write(a, http.StatusPartialContent)
+}
+
+// MultipleChoices multiple choices
+func (r Responder) MultipleChoices(a interface{}) {
+	r.write(a, http.StatusMultipleChoices)
+}
+
+// MovedPermanently moved permanently
+func (r Responder) MovedPermanently(a interface{}) {
+	r.write(a, http.StatusMovedPermanently)
+}
+
+// Found found
+func (r Responder) Found(a interface{}) {
+	r.write(a, http.StatusFound)
+}
+
+// SeeOther see other
+func (r Responder) SeeOther(a interface{}) {
+	r.write(a, http.StatusSeeOther)
+}
+
+// NotModified not modified
+func (r Responder) NotModified(a interface{}) {
+	r.write(a, http.StatusNotModified)
+}
+
+// UseProxy use proxy
+func (r Responder) UseProxy(a interface{}) {
+	r.write(a, http.StatusUseProxy)
+}
+
+// TemporaryRedirect temporary redirect
+func (r Responder) TemporaryRedirect(a interface{}) {
+	r.write(a, http.StatusTemporaryRedirect)
 }
 
 // Error Return custom error
@@ -97,21 +162,116 @@ func (r Responder) Error(err string, code int, errorData interface{}) {
 }
 
 // BadRequest Respond to an HTTP request with BadRequest
-func (r Responder) BadRequest(err string) {
-	r.Error(err, 400, nil)
+func (r Responder) BadRequest(err string, data interface{}) {
+	r.Error(err, http.StatusBadRequest, data)
 }
 
 // Unauthorized Return unauthorized
-func (r Responder) Unauthorized(err string) {
-	r.Error(err, 401, nil)
+func (r Responder) Unauthorized(err string, data interface{}) {
+	r.Error(err, http.StatusUnauthorized, data)
+}
+
+// PaymentRequired payment required
+func (r Responder) PaymentRequired(err string, data interface{}) {
+	r.Error(err, http.StatusPaymentRequired, data)
 }
 
 // NotFound return not found
-func (r Responder) NotFound(err string) {
-	r.Error(err, 404, nil)
+func (r Responder) NotFound(err string, data interface{}) {
+	r.Error(err, http.StatusNotFound, data)
+}
+
+// MethodNotAllowed method not allowed
+func (r Responder) MethodNotAllowed(err string, data interface{}) {
+	r.Error(err, http.StatusMethodNotAllowed, data)
+}
+
+// NotAcceptable not acceptable
+func (r Responder) NotAcceptable(err string, data interface{}) {
+	r.Error(err, http.StatusNotAcceptable, data)
+}
+
+// ProxyAuthenticationRequired proxy authentication required
+func (r Responder) ProxyAuthenticationRequired(err string, data interface{}) {
+	r.Error(err, http.StatusProxyAuthRequired, data)
+}
+
+// RequestTimeout request timeout
+func (r Responder) RequestTimeout(err string, data interface{}) {
+	r.Error(err, http.StatusRequestTimeout, data)
+}
+
+// Conflict conflict
+func (r Responder) Conflict(err string, data interface{}) {
+	r.Error(err, http.StatusConflict, data)
+}
+
+// Gone gone
+func (r Responder) Gone(err string, data interface{}) {
+	r.Error(err, http.StatusGone, data)
+}
+
+// LengthRequired length required
+func (r Responder) LengthRequired(err string, data interface{}) {
+	r.Error(err, http.StatusLengthRequired, data)
+}
+
+// PreconditionFailed precondition failed
+func (r Responder) PreconditionFailed(err string, data interface{}) {
+	r.Error(err, http.StatusPreconditionFailed, data)
+}
+
+// RequestEntityTooLarge request entity too large
+func (r Responder) RequestEntityTooLarge(err string, data interface{}) {
+	r.Error(err, http.StatusRequestEntityTooLarge, data)
+}
+
+// RequestURITooLong request URI too long
+func (r Responder) RequestURITooLong(err string, data interface{}) {
+	r.Error(err, http.StatusRequestURITooLong, data)
+}
+
+// UnsupportedMediaType unsupported media type
+func (r Responder) UnsupportedMediaType(err string, data interface{}) {
+	r.Error(err, http.StatusUnsupportedMediaType, data)
+}
+
+// RequestedRangeNotSatisfiable requested range not satisfied
+func (r Responder) RequestedRangeNotSatisfiable(err string, data interface{}) {
+	r.Error(err, http.StatusRequestedRangeNotSatisfiable, data)
+}
+
+// ExpectationFailed expectation failed
+func (r Responder) ExpectationFailed(err string, data interface{}) {
+	r.Error(err, http.StatusExpectationFailed, data)
 }
 
 // InternalServerError Return internal server error
-func (r Responder) InternalServerError(err string) {
-	r.Error(err, 500, nil)
+func (r Responder) InternalServerError(err string, data interface{}) {
+	r.Error(err, http.StatusInternalServerError, data)
+}
+
+// NotImplemented not implemented
+func (r Responder) NotImplemented(err string, data interface{}) {
+	r.Error(err, http.StatusNotImplemented, data)
+}
+
+// BadGateway bad gateway
+func (r Responder) BadGateway(err string, data interface{}) {
+	r.Error(err, http.StatusBadGateway, data)
+}
+
+// ServiceUnavailable service unavailable
+func (r Responder) ServiceUnavailable(err string, data interface{}) {
+	r.Error(err, http.StatusServiceUnavailable, data)
+}
+
+// GatewayTimeout gateway timeout
+func (r Responder) GatewayTimeout(err string, data interface{}) {
+	r.Error(err, http.StatusGatewayTimeout, data)
+}
+
+// HTTPVersionNotSupported HTTP version not supported
+func (r Responder) HTTPVersionNotSupported(err string, data interface{}) {
+	r.Error(err, http.StatusHTTPVersionNotSupported, data)
 }
